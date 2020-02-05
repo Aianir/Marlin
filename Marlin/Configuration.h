9b533cd98b7fb96682e4bf8925be59a9df844ac1
Hans Erik Lange <hanserik@HE.local>
2020-02-05 20:41:32 +0100

    Adjust steps/mm, was overextruding

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -723,7 +723,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 409 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 395 }
 
 /**
  * Default Max Feed Rate (mm/s)
